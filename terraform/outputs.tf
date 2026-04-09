ef15ab744fdaba251d1b115b6525b284f5fbf3a5
Neitong <Hengneitong.keo@student.cadt.edu.kh>
2026-04-09 16:38:28 +0700

    remove: delete unused rds_endpoint output from Terraform configuration

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -12,8 +12,3 @@ output "s3_website_endpoint" {
   description = "The website endpoint for the frontend"
   value       = aws_s3_bucket_website_configuration.frontend_web.website_endpoint
 }
-
-output "rds_endpoint" {
-  description = "The connection endpoint for the database"
-  value       = aws_db_instance.grand_cineplex_db.endpoint
-}
